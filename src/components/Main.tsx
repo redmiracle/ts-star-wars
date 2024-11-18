70a88910b8c3306461015ca8b8b13c4404dc336a
Maxim <besstrakhov.m@gmail.com>
2024-11-18 21:01:11 +0200

    add routing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,20 +5,31 @@ import Contact from "./Contact.tsx";
 import {navItems} from "../utils/constants.ts";
 import {useContext} from "react";
 import {SWContext} from "../utils/context.ts";
+import {Route, Routes} from "react-router-dom";
 
 const Main = () => {
     const {page} = useContext(SWContext);
 
-    switch (page) {
-        case navItems[1]:
-            return <AboutMe/>;
-        case navItems[2]:
-            return <StarWars/>;
-        case navItems[3]:
-            return <Contact/>;
-        default:
-            return <Home/>;
-    }
+    return (
+        <Routes>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={`/${navItems[0]}`} element={<Home/>}/>
+            <Route path={`/${navItems[1]}`} element={<AboutMe/>}/>
+            <Route path={`/${navItems[2]}`} element={<StarWars/>}/>
+            <Route path={`/${navItems[3]}`} element={<Contact/>}/>
+        </Routes>
+    )
+
+    // switch (page) {
+    //     case navItems[1]:
+    //         return <AboutMe/>;
+    //     case navItems[2]:
+    //         return <StarWars/>;
+    //     case navItems[3]:
+    //         return <Contact/>;
+    //     default:
+    //         return <Home/>;
+    // }
 }
 
 export default Main;
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -5,10 +5,11 @@ interface Props {
 }
 
 const NavItem = ({itemTitle}:Props) => {
-    const {changePage} = useContext(SWContext);
 
     return (
-        <li onClick={() => changePage(itemTitle)} className={`bg-red-color border-black border-2 rounded-[5px] px-3 py-2 cursor-pointer hover:text-white hover:bg-red-500`}>{itemTitle}</li>
+        <li onClick={() => changePage(itemTitle)} className={`bg-red-color border-black border-2 rounded-[5px] px-3 py-2 cursor-pointer hover:text-white hover:bg-red-500`}>
+         <a href={`/${itemTitle}`}>{itemTitle}</a>
+        </li>
     );
 };
 
