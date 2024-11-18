0576c11c9990316d092f9f379618b7ac53802745
Maxim <besstrakhov.m@gmail.com>
2024-11-18 20:07:38 +0200

    correct bag tailWind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    // "./index.html",
-    // "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poller': '"Poller One", serif'
+      },
+      colors: {
+        'base-color': '#eedb00',
+        'grey-color': 'rgba(60, 69, 67, 0.6)',
+        'red-color': 'rgba(204, 3, 35, 0.5)'
+      },
+      backgroundImage: {
+        'sky': 'url(./assets/sky.jpg)'
+      },
+      boxShadow: {
+        'hero': '10px 10px rgba(60, 69, 67, 0.6)'
+      }
+    },
   },
   plugins: [],
 }
