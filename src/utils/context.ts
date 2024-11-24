e18b74d5aee0152292567103b361c7b9352390ee
Maxim <besstrakhov.m@gmail.com>
2024-11-24 16:10:25 +0200

    Update Header

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -2,5 +2,6 @@ import {createContext} from "react";
 import {SWcontextValue} from "./types";
 
 export const SWContext = createContext<SWcontextValue>({
-   headerName:"Luke Skywalker"
+   headerName:"Luke Skywalker",
+   setHeaderName: (name: string) => console.log(name),
 })
